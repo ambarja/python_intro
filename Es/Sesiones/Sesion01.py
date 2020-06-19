8828c28cf836eb34aff2ef041bcf485faddbb3c8
barja8 <antony.barja8@gmail.com>
2020-06-18 20:09:21 -0500

    pyqgis 18jun

--- Es/Sesiones/Sesion01.py
+++ Es/Sesiones/Sesion01.py
@@ -22,6 +22,73 @@ class = 'Programación'
 
 # Tipos de variables básicas en python (númerico, carácteres, lógico)
 
+# Integer
+print(1231213123123123121231234  + 1 )
+print(10)
+
+# Número binario :v 
+print(0b10)
+print(0B10)
+
+# Número octal :'v
+print(0o10)
+print(0O10)
+
+# Número Hexadecimal :''v 
+print(0x10)
+print(0X10)
+
+# Identificando los tipos de objectos en python
+type(0o10)
+type(0b10)
+type(0x10)
+
+type(0.2)
+type(True)
+type('name')
+type(24)
+
+# Números complejos aj + b 
+ 
+complejo = 2j + 3 
+print(complejo, type(complejo))
+
+# Strings
+
+print('Hola mundo')
+print('-----'*12)
+print('2 + 3 es = ', 2+3)
+
+# Especiales secuencias
+
+print("a \tb")
+print("a\nb")
+print("a\\b")
+print("a\fb")
+print("a\bb")
+print("a\vb")
+print('a \N{rightwards arrow}')
+print('\u2192')
+
+# Uso de comillas
+
+print('Hola "Antony" :v ')
+print("Hi my names is 'Antony' and I'm 'data analyst' ")
+
+# Boleanos o lógicos
+type(True)
+print('Do you always lies?\n', True)
+
+# Explorando algunas funciones básicas de python
+
+
+
+
+
+
+# Referencias: https://realpython.com/python-data-types/
+
+
 
 
 
